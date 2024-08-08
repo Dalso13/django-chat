3043fd2cd39356c4f6f991451579a0d0dbeccc1f
Dalso <flskwhdhk@gmail.com>
2024-08-08 22:41:09 +0900

    rebase : Json웹소켓컨슈머를 통해 json 직렬화과정 생략

--- app/consumer.py
+++ app/consumer.py
@@ -1,29 +1,22 @@
 import json
 
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import JsonWebsocketConsumer
 
 
-class LiveBlogConsumer(WebsocketConsumer):
+class LiveBlogConsumer(JsonWebsocketConsumer):
     groups = ['liveBlog']
 
     def liveBlog_post_created(self, event_dict):
-        self.send(text_data=json.dumps(event_dict))
+        self.send_json(event_dict)
 
     def liveBlog_post_updated(self, event_dict):
-        self.send(text_data=json.dumps(event_dict))
+        self.send_json(event_dict)
 
     def liveBlog_post_deleted(self, event_dict):
-        self.send(text_data=json.dumps(event_dict))
+        self.send_json(event_dict)
 
 
-class EchoConsumer(WebsocketConsumer):
-    def receive(self, text_data=None, bytes_data=None):
-        # json데이터 object 로 변환
-        obj = json.loads(text_data)
-        print("수신 : ", obj)
-        # obj 다시 json으로 변환
-        json_string = json.dumps({
-            "content": obj["content"],
-            "user": obj["user"],
-        })
-        self.send(json_string)
+class EchoConsumer(JsonWebsocketConsumer):
+    def receive_json(self, connect, bytes_data=None, **kwargs):
+        print("수신 :", connect)
+        self.send_json(connect)
