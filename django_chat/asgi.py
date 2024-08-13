976c8453ac458d8b07c2e5d4ff3bf413f3eb2a63
Dalso <flskwhdhk@gmail.com>
2024-08-13 22:35:52 +0900

    feat : 인증받은 유저만 웹소켓 접근하게 설정

--- django_chat/asgi.py
+++ django_chat/asgi.py
@@ -1,5 +1,6 @@
 import os
 
+from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
@@ -14,8 +15,10 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     # Just HTTP for now. (We can add other protocols later.)
-    "websocket": URLRouter(
-        app.routing.websocket_urlpatterns +
-        chat.routing.websocket_urlpatterns
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            app.routing.websocket_urlpatterns +
+            chat.routing.websocket_urlpatterns
+        ),
     ),
 })
