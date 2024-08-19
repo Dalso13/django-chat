ace4aa2f561beb22a8877be307391e5850d57013
Dalso <flskwhdhk@gmail.com>
2024-08-19 17:31:08 +0900

    feat: web socket csrf 방어를 위한 AllowedHostsOriginValidator 설정

--- django_chat/asgi.py
+++ django_chat/asgi.py
@@ -2,6 +2,7 @@ import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 import app.routing
@@ -15,10 +16,12 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     # Just HTTP for now. (We can add other protocols later.)
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            app.routing.websocket_urlpatterns +
-            chat.routing.websocket_urlpatterns
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                app.routing.websocket_urlpatterns +
+                chat.routing.websocket_urlpatterns
+            ),
         ),
     ),
 })
